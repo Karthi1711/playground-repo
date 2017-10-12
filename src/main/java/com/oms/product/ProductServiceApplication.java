249a31340193ac5a516e798aa7aa30f15438f328
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2017-10-12 17:04:55 +0530

    Testing Security Enabling With IPC Call

--- src/main/java/com/oms/product/ProductServiceApplication.java
+++ src/main/java/com/oms/product/ProductServiceApplication.java
@@ -4,6 +4,7 @@ import com.oms.common.security.config.EnableOMSSecurity;
 import com.oms.common.web.EnableOMSCommonWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
